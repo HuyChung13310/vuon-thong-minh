18c68c9774358cca47901f5dc178709718d15997
HuyChung13310 <12.cghuy.61@gmail.com>
2026-03-15 05:20:32 -0600

    Refactor getData function and updateData calls

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-const username = "HuyChung";
+const username = "HuyChung"
 
 async function getData(feed){
-    const url = `https://io.adafruit.com/api/v2/${username}/feeds/${feed}/data/last`;
 
-    try{
-        const response = await fetch(url);
-        const data = await response.json();
-        return data.value;
-    }
-    catch(error){
-        console.log("Lỗi đọc dữ liệu:",error);
-        return "--";
-    }
+    const url = `https://io.adafruit.com/api/v2/${username}/feeds/${feed}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.last_value
 }
 
 async function updateData(){
 
-    const temp = await getData("temperature");
-    const soil = await getData("soil");
-    const light = await getData("light");
+    const temp = await getData("temperature")
+    const soil = await getData("soil")
+    const light = await getData("light")
 
-    document.getElementById("temp").innerText = temp + " °C";
-    document.getElementById("soil").innerText = soil + " %";
-    document.getElementById("light").innerText = light;
+    document.getElementById("temp").innerText = temp
+    document.getElementById("soil").innerText = soil
+    document.getElementById("light").innerText = light
 }
 
-setInterval(updateData,3000);
-updateData();
+setInterval(updateData,3000)
+updateData()
